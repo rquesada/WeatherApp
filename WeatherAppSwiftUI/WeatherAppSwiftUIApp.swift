57d932db19b89881c4f0584c9295bc946f1e740e
Roy Quesada <royquesada@Roys-MacBook-Pro.local>
2023-04-06 17:47:59 -0600

    Initial project

---
+++ WeatherAppSwiftUI/WeatherAppSwiftUIApp.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherAppSwiftUIApp.swift
+//  WeatherAppSwiftUI
+//
+//  Created by Mohammad Azam on 3/5/21.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppSwiftUIApp: App {
+    
+    var body: some Scene {
+        WindowGroup {
+            WeatherListScreen()
+        }
+    }
+}
